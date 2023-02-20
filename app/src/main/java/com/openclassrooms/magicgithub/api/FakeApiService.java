7d11a030dc851f0df62b32735fb5defa68b44077
Selena Sean <selena.sean.1994@gmail.com>
2023-02-20 11:25:06 +0100

    TODO2/3 deleteUser fait - test2/3 ok

--- app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiService.java
+++ app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiService.java
@@ -17,7 +17,7 @@ public class FakeApiService implements ApiService {
      */
     @Override
     public List<User> getUsers() {
-       return FAKE_USERS;
+       return users;
     }
 
 
@@ -36,7 +36,6 @@ public class FakeApiService implements ApiService {
      */
     @Override
     public void deleteUser(User user) {
-        // TODO: A modifier
         users.remove(user);
     }
 }
