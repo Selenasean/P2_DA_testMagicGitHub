3b2e7f3833276b2694fd688feecdcba15aabff24
Selena Sean <selena.sean.1994@gmail.com>
2023-02-20 12:06:26 +0100

    TODO3/3 generateRandomUser fait - TEST3/3 ok, TEST instru ok

--- app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiService.java
+++ app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiService.java
@@ -2,8 +2,8 @@ package com.openclassrooms.magicgithub.api;
 
 import com.openclassrooms.magicgithub.model.User;
 import java.util.List;
+import java.util.Random;
 
-import static com.openclassrooms.magicgithub.api.FakeApiServiceGenerator.FAKE_USERS;
 import static com.openclassrooms.magicgithub.api.FakeApiServiceGenerator.FAKE_USERS_RANDOM;
 import static com.openclassrooms.magicgithub.api.FakeApiServiceGenerator.generateUsers;
 
@@ -27,8 +27,11 @@ public class FakeApiService implements ApiService {
      */
     @Override
     public void generateRandomUser() {
-        // TODO: A modifier
+        Random random = new Random();
+        int randomIndex = random.nextInt(FAKE_USERS_RANDOM.size());
+        User getRandomUser = FAKE_USERS_RANDOM.get(randomIndex);
 
+        users.add(getRandomUser);
     }
 
     /**
